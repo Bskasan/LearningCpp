023b98333e18ddcdc54c6b298ff8761f5b4f512d
Bskasan <b.kasan@yandex.com>
2023-09-18 20:36:12 +0300

    up variables primitive types

--- MyFirstApp/MyFirstApp/Source.cpp
+++ MyFirstApp/MyFirstApp/Source.cpp
@@ -32,9 +32,12 @@ int main()
 	float hp;
 	name = "William J. Parker"; // That's the name
 	goldPieces = 322; // start with this much gold
-	hp = 100.0f; // hit points are decimal valued.
+	hp = 70.5f; // hit points are decimal valued.
 
-	cout << "Character " << name << " has " <<
+
+	cout << "Character " << name << " has " << hp << " hp and "<< goldPieces << " gold.";
+
+	//------------- 
 
 
 	return 0;		// "return" to the operating sys

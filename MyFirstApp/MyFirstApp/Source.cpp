f936778810c20570e13cecd14251a0ee1f6e4bf3
Bskasan <b.kasan@yandex.com>
2023-09-18 21:50:46 +0300

    player and vector object created & object types

--- MyFirstApp/MyFirstApp/Source.cpp
+++ MyFirstApp/MyFirstApp/Source.cpp
@@ -2,6 +2,20 @@
 using namespace std; // allows us to write cout
 					 // instad of using std::cout
 
+
+struct Vector // Vector object definition
+{
+	float x, y, z; // x, y, and z positions all floats.
+};
+
+struct Player
+{
+	string pName;
+	string pType;
+	int hp;
+	Vector pPosition;
+};
+
 int main() 
 {
 	cout << "Hello World!" << endl;
@@ -37,7 +51,26 @@ int main()
 
 	cout << "Character " << name << " has " << hp << " hp and "<< goldPieces << " gold.";
 
-	//------------- 
+	//------------- OBJECT TYPES -----------------//
+
+	// We defined Vector above the main method.
+	// The computer now knows what a Vector is.
+	// So we can create one.
+
+	Vector v; // Create a Vector instance called v.
+	v.x = 20, v.y = 30, v.z = 40; // Assign some values.
+	cout << "A 3-space vector at " << v.x << ", " << v.y << ", " << v.z << endl;
+
+	// Insantiating Player object here.
+
+	Player player;
+	player.pName = "Dyson";
+	player.pType = "Warrior";
+	player.pPosition.x = 20;
+	player.pPosition.y = 50;
+	player.pPosition.z = 30;
+	player.hp = 100;
+
 
 
 	return 0;		// "return" to the operating sys

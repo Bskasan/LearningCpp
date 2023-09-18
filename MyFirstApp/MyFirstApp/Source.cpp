16a0b40b5cb33148b421741697213fa2dbce4b25
Bskasan <b.kasan@yandex.com>
2023-09-18 19:47:28 +0300

    learning cpp

---
+++ MyFirstApp/MyFirstApp/Source.cpp
@@ -0,0 +1,17 @@
+# include <iostream> // import input-output library
+using namespace std; // allows us to write cout
+					 // instad of using std::cout
+
+int main() 
+{
+	cout << "Hello World!" << endl;
+	cout << "I am now a C++ programmer. " << endl;
+	cout << "************** **************" << endl;
+	cout << "*************   *************" << endl;
+	cout << "***********       ***********" << endl;
+	cout << "*********           *********" << endl;
+	cout << "***********       ***********" << endl;
+	cout << "*************   *************" << endl;
+	cout << "************** **************" << endl;
+	return 0;		// "return" to the operating sys
+}

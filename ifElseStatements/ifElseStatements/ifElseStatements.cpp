a293941fe178917e7269ffbe0792791952e061e7
Bskasan <b.kasan@yandex.com>
2023-09-19 13:15:27 +0300

    if else up

--- ifElseStatements/ifElseStatements/ifElseStatements.cpp
+++ ifElseStatements/ifElseStatements/ifElseStatements.cpp
@@ -2,19 +2,18 @@
 //
 
 #include <iostream>
+#include <string>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int x = 10;
+    int y = 22;
+
+    // we need to check if they're equal or not
+    // 1 means true, 0 means false.
+    // if you want to see true/false use boolalpha in your code.
+    cout << "Is x equal to y? C++ says: " << boolalpha << (x == y) << endl;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file

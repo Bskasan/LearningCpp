924ae15e83175c274d892863cbfe316154be682f
Bskasan <b.kasan@yandex.com>
2023-09-20 21:00:38 +0300

    while loop done

--- Looping/Looping/Looping.cpp
+++ Looping/Looping/Looping.cpp
@@ -2,19 +2,46 @@
 //
 
 #include <iostream>
+#include <string>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int x = 1;
+
+	while (x <= 5)
+	{
+		cout << "x is " << x << endl;
+		x++;
+	}
+
+	while (x <= 10)
+	{
+		cout << "x is " << x << endl;
+		x++;
+	}
+
+	int y = 10;
+
+	while (y >= 1)
+	{
+		cout << "y is " << y << endl;
+		y--;
+	}
+
+	int z = 1;
+
+	while (z <= 20)
+	{
+		if (z % 2 == 0)
+		{
+			cout << "z is " << z << endl;
+		}
+
+		z++;
+	}
+
+	cout << "Loop Finished!" << endl;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file

8a2f9b7835d1de7f013ea7edf670cf1199b99b47
Bskasan <b.kasan@yandex.com>
2023-09-21 14:19:59 +0300

    cat

--- ClassInheritance/ClassInheritance/ClassInheritance.cpp
+++ ClassInheritance/ClassInheritance/ClassInheritance.cpp
@@ -70,6 +70,34 @@ public:
 	}
 };
 
+class Cat : public Mammal
+{
+public:
+	Cat()
+	{
+		cout << "A cat is born" << endl;
+	}
+
+	~Cat()
+	{
+		cout << "The cat has died" << endl;
+	}
+
+	virtual void talk() override
+	{
+		cout << "Meow!" << endl;
+	}
+
+	// Implements walking for a cat .. same as dog!
+
+	virtual void walk() override
+	{
+		cout << "Left front paw & back right paw, right front paw & back left paw..at the speed of " << speed << endl;
+	}
+};
+
+
+
 
 int main()
 {

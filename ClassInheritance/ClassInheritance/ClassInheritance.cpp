78946935f9df262e6b0032f3cbe8e020f7bbbc42
Bskasan <b.kasan@yandex.com>
2023-09-21 14:17:22 +0300

    dog subclass inherited from mammal

--- ClassInheritance/ClassInheritance/ClassInheritance.cpp
+++ ClassInheritance/ClassInheritance/ClassInheritance.cpp
@@ -42,6 +42,32 @@ public:
 	}
 
 	// pure virtual function, (explained below)
+	virtual void walk() = 0;
+};
+
+// This next line says "class Dog inherits from class Mammal"
+class Dog : public Mammal // : is used for inheritance
+{
+public:
+	Dog() 
+	{
+		cout << "A dog is born!" << endl;
+	}
+
+	~Dog()
+	{
+		cout << "The dog died!" << endl;
+	}
+
+	virtual void talk() override
+	{
+		cout << "Woof!" << endl; // Dogs only say woof!
+	}
+
+	virtual void walk() override
+	{
+		cout << "Left front paw & back right paw, right front paw & back left paw..at the speed of " << speed << endl;
+	}
 };
 
 

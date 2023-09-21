5935fdaa6a92093dc0e977b544c9114b110cd2e2
Bskasan <b.kasan@yandex.com>
2023-09-21 19:05:31 +0300

    array 1 done

--- Arrays/Arrays/Arrays.cpp
+++ Arrays/Arrays/Arrays.cpp
@@ -8,7 +8,18 @@ using namespace std;
 
 int main()
 {
-	
+	int array[5]; // Declare an "array" of 5 integers#
+				  // fill slots 0-4 with values
+
+	array[0] = 10;
+	array[1] = 2;
+	array[2] = 3;
+	array[3] = 4;
+	array[4] = 5;
+
+	// Print out contents
+	for (int i = 0; i < 5; i++)
+		cout << array[i] << endl;
 }
 
 
